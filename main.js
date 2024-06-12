7ae6a068a8b454324ea7f4ae94f73d0f28852fdc
Bruh-and-stuff <122206334+Bruh-and-stuff@users.noreply.github.com>
2024-06-12 18:01:14 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var period = new Audio("period.mp3")
+var meltbitter = new Audio("melt bitter.mp3")
+
+function preload(){
+    period = loadSound("period.mp3")
+    meltbitter = loadSound("melt bitter.mp3")
+}
+
+function setup(){
+    cAnVaS = createCanvas(600, 600)
+    cAnVaS.position(1000, 150)
+
+    mYvIdEoYaY = createCapture(VIDEO)
+    mYvIdEoYaY.hide()
+}
+
+function draw(){}
